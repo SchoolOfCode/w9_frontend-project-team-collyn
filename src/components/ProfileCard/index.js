f4f4883be4bf05e1a4f3759b56d67b89146a27d0
elawilkinson <elaw454@gmail.com>
2022-06-22 14:21:39 +0100

    individual profile cards created

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./index.css";
 
+import ProfileOne from "./ProfileOne";
+import ProfileTwo from "./ProfileTwo";
+import ProfileThree from "./ProfileThree";
+
 export default function ProfileCard() {
   return (
-    <div className="card">
-    <div className="image-container">
-      <img src="https://upload.wikimedia.org/wikipedia/en/8/8c/Cocktail_1988.jpg" alt="SoC character" />
-    </div>
-      <h3>Collyn</h3>
-      <p className="project-interest">Project interest: Sports</p>
-      <p className="project-type">Project Type: To-do list</p>
-      <p className="strength">Strength: Frontend</p>
-      <p className="availability">Availability: Weekday evenings</p>
-      <button>Collabor8</button>
-    </div>
+    <>
+      <ProfileOne />
+      <ProfileTwo />
+      <ProfileThree />
+     </>
   );
 }
