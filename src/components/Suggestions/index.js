5a71ab9dd62b2598f3dd03dd605af186386fb7dc
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-06-23 11:34:53 +0100

    changed suggestions

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Suggestions() {
+  // const [input, setInput] = useState('');
+
+  // const handleChange = (event) => {
+  //   setInput(event.target.value);
+  //   console.log('value is:', event.target.value);
+  // };
+  const initialContact = {
+    name: '',
+    project_interest: '',
+    project_type: '',
+    strength: '',
+    availablity: '',
+  };
+  const [contact, setContact] = useState(initialContact);
+
+  const handleChangeFor = (propertyName) => (event) => {
+    setContact((contact) => ({
+      ...contact,
+      [propertyName]: event.target.value,
+    }));
+    console.log(contact.name);
+  };
+
+  async function sendInput(contact) {
+    try {
+      const data = await fetch('http://localhost:5000/projects', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: contact.name,
+          project_interest: contact.project_interest,
+          project_type: contact.project_type,
+          strength: contact.strength,
+          availability: contact.availability,
+          img: '',
+        }),
+      });
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   return (
     <div className="suggestions-box">
       <h3 id="suggestions-intro">
@@ -9,7 +51,11 @@ function Suggestions() {
       <form>
         <div className="suggestions-input">
           <label value="name">Your name</label>
-          <input type="text"></input>
+          <input
+            value={contact.name}
+            onChange={handleChangeFor('name')}
+            type="text"
+          ></input>
         </div>
         <div className="suggestions-input">
           <label value="email">Your email</label>
@@ -17,22 +63,40 @@ function Suggestions() {
         </div>
         <div className="suggestions-input">
           <label value="project_interest">Project Interest</label>
-          <input type="text"></input>
+          <input
+            value={contact.project_interest}
+            onChange={handleChangeFor('project_interest')}
+            type="text"
+          ></input>
         </div>
         <div className="suggestions-input">
           <label value="name">Project Type</label>
-          <input type="text"></input>
+          <input
+            value={contact.project_type}
+            onChange={handleChangeFor('project_type')}
+            type="text"
+          ></input>
         </div>
         <div className="suggestions-input">
           <label value="name">Availablity</label>
-          <input type="text"></input>
+          <input
+            value={contact.availablity}
+            onChange={handleChangeFor('availablity')}
+            type="text"
+          ></input>
         </div>
         <div className="suggestions-input">
           <label value="name">Strength</label>
-          <input type="text"></input>
+          <input
+            value={contact.strength}
+            onChange={handleChangeFor('strength')}
+            type="text"
+          ></input>
         </div>
       </form>
-      <button className="suggestions-button">Send suggestion</button>
+      <button className="suggestions-button" onClick={() => sendInput(contact)}>
+        Send suggestion
+      </button>
     </div>
   );
 }
